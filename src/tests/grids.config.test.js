4e53bb7a48ba272151a6488bc2fc9f998b63b60f
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-05-06 13:04:56 -0700

    test: add testing for grids.config.alias

--- src/tests/grids.config.test.js
+++ src/tests/grids.config.test.js
@@ -1,5 +1,51 @@
 // @ts-check
 
-describe('Initial Testing', () => {
+const oparea = require('../index')
+
+describe('Alias Function Testing', () => {
+    test.each([null, undefined, ''])('Alias should throw error on: %p', (input) => {
+        expect(() => {
+            // eslint-disable-next-line no-unused-vars
+            const result = oparea.grids.config.alias(input, 'ValidName')
+        }).toThrow(/null, undefined, or empty strings/)
+    })
+
+    test('Single refersTo creates correct object', () => {
+        const alias = 'MANTA'
+        const refersTo = 'A5'
+        const output = { alias: 'MANTA', refersTo: ['A5'] }
+        expect(oparea.grids.config.alias(alias, refersTo)).toEqual(output)
+    })
+
+    test('Multiple refersTo creates correct object', () => {
+        const alias = 'COFFEE'
+        const refersTo = ['BLACK', 'ALIEN', 'G22']
+        const output = { alias: 'COFFEE', refersTo: ['BLACK', 'ALIEN', 'G22'] }
+        expect(oparea.grids.config.alias(alias, refersTo)).toEqual(output)
+    })
+
+    test('Duplicate refersTo ignored in created object', () => {
+        const alias = 'COFFEE'
+        const refersTo = ['BLACK', 'ALIEN', 'G22', 'BLACK']
+        const output = { alias: 'COFFEE', refersTo: ['BLACK', 'ALIEN', 'G22'] }
+        expect(oparea.grids.config.alias(alias, refersTo)).toEqual(output)
+    })
+
+    test.each([null, undefined, ''])('RefersTo should be empty array on: %p', (input) => {
+        const alias = 'COFFEE'
+        const output = { alias: 'COFFEE', refersTo: [''] }
+        expect(oparea.grids.config.alias(alias, input)).toEqual(output)
+    })
+})
+
+describe('Initial aliasCollection Testing', () => {
+    test.todo('Add initial tests')
+})
+
+describe('Initial geographicBoundaries Testing', () => {
+    test.todo('Add initial tests')
+})
+
+describe('Initial geographicConfig Testing', () => {
     test.todo('Add initial tests')
 })
