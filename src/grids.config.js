089e6eebd21431e68376d8764a0f20f64d9f0f86
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-05-06 13:03:09 -0700

    feat: add grids.config.alias function
    Initial placeholder code. Now that the infrastructure is in place, I can begin fleshing it out.

--- src/grids.config.js
+++ src/grids.config.js
@@ -14,6 +14,7 @@
  * Duplicate strings will consolidate to a single value.
  * An alias name cannot correspond to itself.
  * @returns {GridConfig.Alias} Properly formatted alias object
+ * @throws {Error} Alias names cannot be null, undefined, or empty strings.
  * @example
  * const alias1 = alias('MANTA', 'A5') // { alias: 'MANTA', refersTo: ['A5'] }
  * const alias2 = alias('COFFEE', ['BLACK', 'ALIEN', 'G22']) // { alias: 'COFFEE', refersTo: ['BLACK', 'ALIEN', 'G22'] }
@@ -21,12 +22,29 @@
  * const alias4 = alias('RAY', ['COFFEE', 'MANTA', 'A6', 'B5', 'B6']) // { alias: 'RAY', refersTo: ['COFFEE', 'MANTA', 'A6', 'B5', 'B6'] }
  */
 function alias(aliasName, refersToNames) {
-    return { alias: aliasName, refersTo: [refersToNames] }
-}
+    // Null inputs should throw an error
+    if (aliasName === null || aliasName === undefined || aliasName === '') {
+        throw new Error('Alias names and referrels cannot be null, undefined, or empty strings.')
+    }
 
-// * const collectionTwo = [{ alias: 'MANTA', refersTo: ['A5'] },
-// *                        { alias: 'COFFEE', refersTo: ['BLACK', 'ALIEN', 'G22'] },
-// *                        { alias: 'RAY', refersTo: ['COFFEE', 'MANTA', 'A6', 'B5', 'B6'] }]
+    // Ensure the refersToNames are contained in an array under all cases
+    let processedArray = []
+    if (Array.isArray(refersToNames)) {
+        // Already in an array
+        processedArray = refersToNames
+    } else if (refersToNames) {
+        // Only a single name
+        processedArray.push(refersToNames)
+    } else {
+        // Passed a falsy value
+        processedArray.push('')
+    }
+
+    // Remove duplicates
+    processedArray = [...new Set(processedArray)]
+
+    return { alias: aliasName, refersTo: processedArray }
+}
 
 /**
  * Combines an alias or alias collection into a combined alias collection.
