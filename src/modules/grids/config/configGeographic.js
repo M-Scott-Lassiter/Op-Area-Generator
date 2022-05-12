1a9dbd404da7ce5a8c6c50a7dad8376e8188d780
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-05-12 08:03:06 -0700

    feat: add the `configGeographic` function to the grids.config object
    This function is used to create a complete and valid geographic grid configuration object. The
    boundaries are required, however if called by itself with no options it will return a valid default
    object.

--- src/modules/grids/config/configGeographic.js
+++ src/modules/grids/config/configGeographic.js
@@ -1,27 +1,51 @@
-/**
- * Provides helper functions used to create valid configuration objects to generate grids.
- *
- *
- */
+const { validConfigGeographic } = require('../validation/validConfigGeographic')
+// const { aliasCollection } = require('./aliasCollection')
+const { axisOptions } = require('./axisOptions')
+const { boundaries } = require('./boundaries')
+const { naming } = require('./naming')
 
 /**
- * Takes an input of a geographic grid boundary an optional parameters and returns a fully formatted {@link GridConfig.GeographicGridConfig} object
- * ready for use with <code>oparea.grids.geographic()</code>. Unused options will return the default. Invalid options will get ignored.
+ * Takes an optional object input of the various parameters required for a valid geographic grid configuration and returns a fully
+ * formatted {@link GridConfig.GeographicGridConfig} object ready for use with <code>oparea.grids.geographic()</code>.
+ * Any parameters left empty will return the default values for each data set. Invalid options will throw an error.
  *
  * @memberof Grids.Config
- * @param {Grids.Definitions.Boundaries} boundaries Geographic grid boundaries
  * @param {object} [options] Optional additional parameters. If omitted, this function returns default
- * @param {	Grids.Definitions.AxisOptions} [options.naming] A grid naming configuration object
- * @param {	Grids.Definitions.AxisOptions} [options.longitudeAxis] Longitude axis configuration object
- * @param {	Grids.Definitions.AxisOptions} [options.latitudeAxis] Latitude axis configuration object
- * @param {	Grids.Definitions.OmittedGridSquares} [options.omits] Grid squares to exclude from this configuration
- * @param {	Grids.Definitions.Alias | Grids.Definitions.AliasCollection} [options.aliases] Defines aliases for grid squares
- * @returns {Grids.Definitions.GeographicGridConfig} A properly formatted grid configuration object ready to use with {@link Grids.Geoggraphic}
- * @throws {Error} Invalid boundaries object
+ * @param {       Grids.Definitions.Boundaries} boundaries Geographic grid boundaries. This property is required if options are included
+ * @param {       Grids.Definitions.Naming} [options.naming] A grid naming configuration object
+ * @param {       Grids.Definitions.AxisOptions} [options.longitudeAxis] Longitude axis configuration object
+ * @param {       Grids.Definitions.AxisOptions} [options.latitudeAxis] Latitude axis configuration object
+ * @param {       Grids.Definitions.Omits} [options.omits] Grid squares to exclude from this configuration
+ * @param {       Grids.Definitions.Alias | Grids.Definitions.AliasCollection} [options.aliases] Defines aliases for grid squares
+ * @returns {Grids.Definitions.GeographicGridConfig} A properly formatted grid configuration object ready to use with {@link Grids.Geographic}
+ * @throws {Error} Invalid boundaries object | Invalid naming object | Invalid longitudeAxis object | Invalid latitudeAxis object |
+ * Invalid omits object | Invalid alias or aliasCollection object
+ * @throws {RangeError} Minimum latitude and longitude spacing must be at least 1 arc second
  */
-function configGeographic(boundaries, options) {
-    const formattedConfig = options
-    return { formattedConfig }
+function configGeographic(options) {
+    const formattedConfig = { ...options }
+
+    formattedConfig.boundaries = formattedConfig?.boundaries ?? boundaries()
+    formattedConfig.naming = formattedConfig?.naming ?? naming()
+    formattedConfig.longitudeAxis = formattedConfig?.longitudeAxis ?? axisOptions()
+    formattedConfig.latitudeAxis = formattedConfig?.latitudeAxis ?? axisOptions()
+    formattedConfig.omits = formattedConfig?.omits ?? []
+    formattedConfig.aliases = formattedConfig?.aliases ?? []
+
+    // Calling the function with no argument should return a default object. Otherwise, the options must be valid.
+    if (options === undefined) {
+        return formattedConfig
+    }
+    if (typeof options !== 'object' || Array.isArray(options) || options === null) {
+        throw new Error('Options must be an object with valid grid definition properties')
+    }
+
+    // Validation
+    if (!validConfigGeographic(options)) {
+        throw new Error('Options must be an object with valid grid definition properties')
+    }
+
+    return formattedConfig
 }
 
 exports.configGeographic = configGeographic
--- src/modules/grids/tests/configGeographic.test.js
+++ src/modules/grids/tests/configGeographic.test.js
@@ -1,5 +1,146 @@
-// const oparea = require('../../../index')
+const { configGeographic } = require('../config/configGeographic')
+const data = require('./grids.testdata')
+
+const bounds = data.defaultBoundariesValue
 
 describe('Function `configGeographic` Testing', () => {
-    test.todo('Add initial tests')
+    test.each(data.knownBadValues)(
+        'Throws Error on known bad input: configGeographic(%p)',
+        (input) => {
+            expect(() => {
+                configGeographic(input)
+            }).toThrow(/valid grid definition/)
+        }
+    )
+    test('Returns a default object if called with no arguments. Returns default: configGeographic()', () => {
+        expect(configGeographic()).toEqual(data.defaultGeographicConfig)
+    })
+
+    test('Optional boundaries object must be valid. Returns valid: configGeographic( {boundaries: { west: -10, south: -20, east: 10, north: 20 }} )', () => {
+        expect(configGeographic({ boundaries: data.defaultBoundariesValue })).toEqual(
+            data.defaultGeographicConfig
+        )
+    })
+
+    test.each([...data.knownBadValues, { west: -10, south: -20, east: 10, north: 200 }])(
+        'Optional boundaries object must be valid. Throws error: configGeographic( {boundaries: %p} )',
+        (input) => {
+            expect(() => {
+                configGeographic({ boundaries: input })
+            }).toThrow(/valid grid definition/)
+        }
+    )
+
+    test('Optional naming object must be valid. Returns valid: configGeographic( {boundaries: bounds, naming: {<default>}} )', () => {
+        expect(configGeographic({ boundaries: bounds, naming: data.defaultNamingObject })).toEqual(
+            data.defaultGeographicConfig
+        )
+    })
+
+    test.each(data.knownBadValues)(
+        'Optional naming object must be valid. Throws error: configGeographic( {boundaries: bounds, naming: %p} )',
+        (input) => {
+            expect(() => {
+                configGeographic({ boundaries: bounds, naming: input })
+            }).toThrow(/valid grid definition/)
+        }
+    )
+
+    test('Optional longitudeAxis object must be valid. Returns valid: configGeographic( {boundaries: bounds, longitudeAxis: {<default>}} )', () => {
+        expect(
+            configGeographic({ boundaries: bounds, longitudeAxis: data.defaultAxisObject })
+        ).toEqual(data.defaultGeographicConfig)
+    })
+
+    const longitudeSpacingTooSmall = {
+        boundaries: { north: 10, south: 0, east: 1, west: 0 }, // Must be at least one arc second
+        longitudeAxis: { quantity: 10000 }
+    }
+    test.each([...data.knownBadValues, longitudeSpacingTooSmall])(
+        'Optional longitudeAxis object must be valid. Throws error: configGeographic( {boundaries: bounds, longitudeAxis: %p} )',
+        (input) => {
+            expect(() => {
+                configGeographic({ boundaries: bounds, longitudeAxis: input })
+            }).toThrow(/valid grid definition/)
+        }
+    )
+
+    test('Optional latitudeAxis object must be valid. Returns valid: configGeographic( {boundaries: bounds, latitudeAxis: {<default>}} )', () => {
+        expect(
+            configGeographic({ boundaries: bounds, latitudeAxis: data.defaultAxisObject })
+        ).toEqual(data.defaultGeographicConfig)
+    })
+
+    const latitudeSpacingTooSmall = {
+        boundaries: { north: 1, south: 0, east: 0, west: 10 }, // Must be at least one arc second
+        latitudeAxis: { quantity: 10000 }
+    }
+    test.each([...data.knownBadValues, latitudeSpacingTooSmall])(
+        'Optional latitudeAxis object must be valid. Throws error: configGeographic( {boundaries: bounds, latitudeAxis: %p} )',
+        (input) => {
+            expect(() => {
+                configGeographic({ boundaries: bounds, latitudeAxis: input })
+            }).toThrow(/valid grid definition/)
+        }
+    )
+
+    test.each(data.validOmits)(
+        'Optional omits array must be valid. Returns valid: configGeographic( {boundaries: bounds, omits: %p )',
+        (input) => {
+            const testOutput = { ...data.defaultGeographicConfig }
+            testOutput.omits = input
+            expect(configGeographic({ boundaries: bounds, omits: input })).toEqual(testOutput)
+        }
+    )
+
+    test.each(data.invalidOmits)(
+        'Optional omits array must be valid. Throws error: configGeographic( {boundaries: bounds, omits: %p} )',
+        (input) => {
+            expect(() => {
+                configGeographic({ boundaries: bounds, omits: input })
+            }).toThrow(/valid grid definition/)
+        }
+    )
+
+    test.each(data.validAliases)(
+        'Optional aliases must be valid. Returns valid: configGeographic( {boundaries: bounds, aliases: %p )',
+        (input) => {
+            const testOutput = { ...data.defaultGeographicConfig }
+            testOutput.aliases = input
+            expect(configGeographic({ boundaries: bounds, aliases: input })).toEqual(testOutput)
+        }
+    )
+
+    test.each(data.knownBadValues)(
+        'Optional aliases must be valid. Throws error: configGeographic( {boundaries: bounds, aliases: %p} )',
+        (input) => {
+            expect(() => {
+                configGeographic({ boundaries: bounds, aliases: input })
+            }).toThrow(/valid grid definition/)
+        }
+    )
+
+    const aliasCollection = [...data.validAliases]
+    aliasCollection.push([data.alias1, data.alias2])
+    aliasCollection.push(data.validAliasCollection)
+    test.each(aliasCollection)(
+        'Optional alias collections must be valid. Returns valid: configGeographic( {boundaries: bounds, aliases: [%p] )',
+        (input) => {
+            const testOutput = { ...data.defaultGeographicConfig }
+            testOutput.aliases = input
+            console.log(input)
+            expect(configGeographic({ boundaries: bounds, aliases: input })).toEqual(testOutput)
+        }
+    )
+
+    const invalidAliasCollection = [...data.invalidOmits] // While these are invalid omit options, they are also invalid alias collections. Reusing
+    invalidAliasCollection.push([data.alias1, data.alias3]) // Reuses alias key
+    test.each(invalidAliasCollection)(
+        'Optional alias collections must be valid. Throws error: configGeographic( {boundaries: bounds, aliases: [%p]} )',
+        (input) => {
+            expect(() => {
+                configGeographic({ boundaries: bounds, aliases: [input] })
+            }).toThrow(/valid grid definition/)
+        }
+    )
 })
